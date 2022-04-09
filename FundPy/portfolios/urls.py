03543a9ef3461d72484bda6f589190b8d3e1ee2b
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-04-09 13:34:49 +0200

    Basics

--- FundPy/portfolios/urls.py
+++ FundPy/portfolios/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import include, re_path
 from portfolios import views
 
 
 urlpatterns = [
-    url(r'^api/portfolios$',views.portfolio_list),
-    url(r'^api/portfolios/(?P<pk>[0-9]+)$',views.portfolio_detail),
-    url(r'^api/portfolios/published$',views.portfolio_list_published)
+    re_path(r'^api/portfolios$',views.portfolio_list),
+    re_path(r'^api/portfolios/(?P<pk>[0-9]+)$',views.portfolio_detail),
+    re_path(r'^api/portfolios/published$',views.portfolio_list_published)
 ]
--- FundPy/portfolios/views.py
+++ FundPy/portfolios/views.py
@@ -1,3 +1,28 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Portfolio
 
 # Create your views here.
+@api_view(['GET','POST','DELETE'])
+def portfolio_list(request):
+    # GET list of portfolios, POST a new portfolio, DELETE all portfolios
+    return 0
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def portfolio_detail(request, pk):
+    # find portfolio by pk (id)
+    try: 
+        portfolio = Portfolio.objects.get(pk=pk) 
+    except Portfolio.DoesNotExist: 
+        return JsonResponse({'message': 'The portfolio does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+ 
+    # GET / PUT / DELETE portfolio
+    
+        
+@api_view(['GET'])
+def portfolio_list_published(request):
+    # GET all published portfolio
+    return 0
